43e7f39a9b8febc253228f6b9c90a74d92234fc8
victor0198 <victor0198@gmail.com>
2019-03-22 02:09:21 +0200

    output-ul sortat

--- src/anagramsChecker/anagrams.java
+++ src/anagramsChecker/anagrams.java
@@ -4,16 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.Collator;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Collections;
 
 public class anagrams {
-	
+			
 	public static int[] dinStringInArray(String cuvant) {
-		int[] a = new int[20]; 
+		int[] a = new int[30]; 
 		for (int i = 0; i < cuvant.length(); i++) {
     		a[i] = (int)cuvant.charAt(i);
     	}
@@ -22,15 +19,16 @@ public class anagrams {
 	
 	public static int[] sorteazaArray(int[] a, int len) {
 		int minim, pozitia=0;
-    	for (int i = 0; i < len; i++)
+    	for (int i = 0; i < len-1; i++)
         {
             minim = a[i];
-            for (int j = i + 1; j < len; j++)
-                if (minim > a[j]) {
+            for (int j = i + 1; j < len; j++) {
+            	if (minim > a[j]) {
                 	minim = a[j];
                 	pozitia = j;
                 }
-            
+            }
+                
             if(minim < a[i]) {
             	a[pozitia] = a[i];
             	a[i] = minim;
@@ -48,14 +46,54 @@ public class anagrams {
 	}
 	
 	public static void printeazaLista(List<String> lista) {
-		//printarea setului de anagrame in ordinea alfabetica
-		Collections.sort(lista, Collator.getInstance());
 		if(lista.size() > 0) {
 			for (int i = 0; i < lista.size(); i++) {
 				System.out.print(lista.get(i) + " ");
 			}
 			System.out.println();
 		}
+	}
+	
+	public static List<String> sorteazaListaString(List<String> lista) {
+		String inferior;
+		int pozitia=0;
+    	for (int i = 0; i < lista.size(); i++)
+        {
+    		inferior = lista.get(i);
+            for (int j = i + 1; j < lista.size(); j++) {
+            	if (comparaCuvintele(inferior,lista.get(j)) < 0) {
+                	inferior = lista.get(j);
+                	pozitia = j;
+              }
+            }
+            
+            if(comparaCuvintele(inferior, lista.get(i)) > 0) {
+            	lista.set(pozitia, lista.get(i));
+            	lista.set(i, inferior);
+            }
+        }
+		
+		return lista;
+	}
+	
+	public static int comparaCuvintele(String cuvant1, String cuvant2) {
+		int rezultat = 0;
+		
+		if(cuvant1.length() != cuvant2.length()) {
+			return 2;
+		}else {
+			for(int i = 0; i < cuvant1.length(); i++) {		
+				if((int)cuvant1.charAt(i)>(int)cuvant2.charAt(i)) {
+					rezultat = -1;
+					break;
+				}
+				if((int)cuvant1.charAt(i)<(int)cuvant2.charAt(i)) {
+					rezultat = 1;
+					break;
+				}
+			}
+		}
+		return rezultat;
 	}
 	
 	public static void main(String [] args) {
@@ -76,7 +114,7 @@ public class anagrams {
             	
             	linia_sortata = "";
             	
-            	//stocarea unui cuvant intr-un array de tip int
+            	//stocarea unui cuvant intr-un array de tip int (selectie)
             	int[] a = dinStringInArray(linia_preluata);
             	
             	//sortarea alfabetica a literelor din cuvant
@@ -84,19 +122,25 @@ public class anagrams {
             	
             	//convertirea array-ului in cuvant (cu literele sortate alfabetic)
             	linia_sortata = dinArrayInString(a, linia_preluata.length());
-        		
-            	if(Objects.equals(linia_precedenta_sortata, linia_sortata)) {
-            		if(primaAnagrama == true) {
+            	
+            	if(comparaCuvintele(linia_precedenta_sortata, linia_sortata) == 0) {
+            		if(primaAnagrama == true) {               //daca a fost identificata anagrama, exclusiv prima
             			setDeAnagrame.add(linia_precedenta);
             		}
             		setDeAnagrame.add(linia_preluata);
             		
             		primaAnagrama = false;
             		
-                }else {
+                }else {                                       
                 	primaAnagrama = true;
         			
-                	printeazaLista(setDeAnagrame);
+                	if(setDeAnagrame.size() > 0) {
+                		//sortarea lstei cu datagrame in ordine alfabetica
+                    	setDeAnagrame = sorteazaListaString(setDeAnagrame);
+                    	
+                    	//printarea setului de anagrame
+                    	printeazaLista(setDeAnagrame);
+                	}
                 	
         			setDeAnagrame.clear();
         		}
